906c0b0660717ef76f0c1afbb88a5bbce335b74c
SunflashRune <102254912+SunflashRune@users.noreply.github.com>
2024-03-26 15:14:37 -0500

    Initial Creation
    Maybe releases will work now?

---
+++ README.md
@@ -0,0 +1,2 @@
+# Manual Skyrim Vanilla PopTracker Pack for AP
+ A Poptracker Pack for the Manual AP Randomizer of Skyrim Vanilla
---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,32 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [16867136000] = {"MainQuestProg", "consumable"},
+    [16867136001] = {"FactionComplete", "consumable"},
+    [16867136002] = {"CompanionProg", "consumable"},
+    [16867136003] = {"CollegeProg", "consumable"},
+    [16867136004] = {"GuildProg", "consumable"},
+    [16867136005] = {"Conjuration", "toggle"},
+    [16867136006] = {"Destruction", "toggle"},
+    [16867136007] = {"Archery", "toggle"},
+    [16867136008] = {"OneHanded", "toggle"},
+    [16867136009] = {"TwoHanded", "toggle"},
+    [16867136010] = {"Speech","toggle"},
+    [16867136011] = {"Alterattion","toggle"},
+    [16867136012] = {"Restoration","toggle"},
+    [16867136013] = {"Block","toggle"},
+    [16867136014] = {"HeavyArmor","toggle"},
+    [16867136015] = {"LightArmor","toggle"},
+    [16867136016] = {"Sneak","toggle"},
+    [16867136017] = {"Enchanting","toggle"},
+    [16867136018] = {"Illusion","toggle"},
+    [16867136019] = {"Smithing","toggle"},
+    [16867136020] = {"Alchemy","toggle"},
+    [16867136021] = {"Lockpicking","toggle"},
+    [16867136022] = {"Pickpocket","toggle"},
+    [16867136023] = {"WeaponProg","consumable"},
+    [16867136024] = {"LightArmProg","consumable"},
+    [16867136025] = {"HeavyArmProg","consumable"},
+    [16867136026] = {"SpellProg","consumable"}
+}
